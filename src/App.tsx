92b5d69aa8a765491e72309f4db234edf0675542
Minong <minhk3259@gmail.com>
2019-08-13 18:13:56 +0900

    init 및 Index 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import Index from './components/Index/Index';
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">{'  '}</div>
+      <Index text={['WELCOME', 'TO', 'MUSIC', 'CHARTS']} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Index/Index.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useEffect } from 'react';
+
+import styles from './Index.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  text: string[];
+  isVisible?: boolean;
+}
+
+const Index: FC<Props> = ({ text, isVisible = true }) => {
+  useEffect(() => {}, [isVisible]);
+
+  const elements = [];
+  let idx = 0;
+  for (const c of text) {
+    elements.push(
+      <span key={idx++} className={cx(`line-${idx}`)}>
+        {c}
+      </span>,
+    );
+  }
+
+  return (
+    <div className={cx('modal', { show: isVisible, hide: !isVisible })}>
+      {elements}
+    </div>
+  );
+};
+
+export default Index;
